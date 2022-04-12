65ca8124c87ffa2ad297fa22260a39d0ff5cf893
Antonio Paez <paezha@gmail.com>
2022-04-12 08:49:29 -0400

    Copied raw data and initialized pkgdown, license, documentation

--- R/package-creation-steps.R
+++ R/package-creation-steps.R
@@ -1,4 +1,23 @@
 # Create package
 usethis::create_package("C:/Antonio/packages/capSantiago")
 
+# Add support script to build ignore
+usethis::use_build_ignore("package-creation-steps.R")
+
 # Use git
+usethis::use_git()
+
+# Use github
+usethis::use_github()
+
+# Create data-raw folder
+usethis::use_data_raw()
+
+# Initialize license
+usethis::use_mit_license()
+
+# Initialize documentation file
+usethis::use_package_doc()
+
+# Initialize pkgdown
+usethis::use_pkgdown_github_pages()
